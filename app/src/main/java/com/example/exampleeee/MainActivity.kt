2075c3890e797f55ff01016a5dcbf125f59530e5
CASPER <hkubratkn@gmail.com>
2022-05-05 17:51:07 +0300

    onBoarding

--- app/src/main/java/com/example/exampleeee/MainActivity.kt
+++ app/src/main/java/com/example/exampleeee/MainActivity.kt
@@ -1,53 +1,216 @@
 package com.example.exampleeee
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import com.example.exampleeee.ui.theme.ExampleeeeTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.exampleeee.ui.theme.Shapes
+import com.google.accompanist.pager.*
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExampleeeeTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    CoilImage()
-                }
+                val navController = rememberNavController()
+                SetUpNavGraph(navController = navController)
             }
         }
     }
 }
 
+@HiltAndroidApp
+class MyApplication : Application()
+
+sealed class Screen(val route: String){
+    object Welcome : Screen(route = "welcome_screen")
+    object Home : Screen(route = "home_screen")
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
 @Composable
-fun CoilImage(){
-    Box(
+fun SetUpNavGraph(
+    navController : NavHostController
+){
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.route
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun WelcomScreen(
+    navController: NavHostController
+){
+    val pages = listOf(
+        OnBoardingPage.First,
+        OnBoardingPage.Second,
+        OnBoardingPage.Third
+    )
+
+    val pagerState = rememberPagerState()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        HorizontalPager(
+            modifier = Modifier.weight(10f),
+            count = 3,
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { position->
+            PagerScreen(onBoardingPage = pages[position])
+        }
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(1f),
+            pagerState = pagerState,
+            activeColor = MaterialTheme.colors.secondary,
+            inactiveColor = MaterialTheme.colors.error,
+            indicatorShape = Shapes.large
+        )
+        FinishButton(
+            modifier = Modifier.weight(1f),
+            pagerState = pagerState
+        ) {
+            navController.popBackStack()
+            navController.navigate(Screen.Home.route)
+        }
+    }
+}
+
+@Composable
+fun PagerScreen(onBoardingPage : OnBoardingPage){
+    Column(
         modifier = Modifier
-            .height(200.dp)
-            .width(200.dp),
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = onBoardingPage.image),
+            contentDescription = "pager image"
+        )
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            text = onBoardingPage.title,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .padding(top = 20.dp),
+            text = onBoardingPage.description,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState : PagerState,
+    onClick: () -> Unit
+){
+    Row(
+        modifier = modifier.padding(horizontal = 40.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ){
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxWidth(),
+            visible = pagerState.currentPage == 2
+        ) {
+            Button(
+                onClick = onClick,
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White
+                )
+            ){
+                Text(text = "Finish")
+            }
+        }
+    }
+}
+
+sealed class OnBoardingPage(
+    @DrawableRes
+    val image : Int,
+    val title : String,
+    val description : String
+){
+    object First : OnBoardingPage(
+        image = R.drawable.ic_launcher_foreground,
+        title = "First",
+        description = "first description"
+    )
+    object Second : OnBoardingPage(
+        image = R.drawable.ic_launcher_background,
+        title = "Second",
+        description = "Second description"
+    )
+    object Third : OnBoardingPage(
+        image = R.drawable.ic_launcher_foreground,
+        title = "Third",
+        description = "Third description"
+    )
+}
+
+@Composable
+fun HomeScreen(){
+    Box(
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ){
-        val painter = rememberImagePainter(
-            data = "https://avatars.githubusercontent.com/u/14994036?v=4",
-            builder = {
-
-
-            }
+        Text(
+            text ="Home",
+            fontSize = MaterialTheme.typography.h4.fontSize
         )
-        Image(
-            painter = painter,
-            contentDescription = "null"
-        )
-
     }
 }
