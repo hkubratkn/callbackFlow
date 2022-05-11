9c0a73c4d7054b9709f6bb572aa02b679b1ae3bd
CASPER <hkubratkn@gmail.com>
2022-05-11 21:33:37 +0300

    menu

--- app/src/main/java/com/example/exampleeee/MainActivity.kt
+++ app/src/main/java/com/example/exampleeee/MainActivity.kt
@@ -1,120 +1,163 @@
 package com.example.exampleeee
 
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 import com.example.exampleeee.ui.theme.ExampleeeeTheme
-import android.graphics.ImageDecoder
-import android.os.Build
-import android.provider.MediaStore
-import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.unit.dp
-import com.example.exampleeee.ui.theme.Purple500
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import java.util.*
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ExampleeeeTheme {
-                PickPhoto()
+                val navController = rememberNavController()
+                var showBottomBar by rememberSaveable { mutableStateOf(true) }
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+                showBottomBar = when (navBackStackEntry?.destination?.route) {
+                    "RouteOfScreenA" -> false // on this screen bottom bar should be hidden
+                    "RouteOfScreenB" -> false // here too
+                    BottomBarScreen.Librry.route -> false
+                    else -> true // in all other cases show bottom bar
+                }
+
+                Scaffold(
+                    modifier = Modifier,
+                    bottomBar = { if (showBottomBar) BottomBar(navController = navController) }
+                ) { innerPadding ->
+                    BottomNavGraph(
+                        navController = navController
+                    )
+                }
+
             }
         }
     }
 }
 
 @Composable
-fun PickPhoto(){
-    val context = LocalContext.current
-    val storage = Firebase.storage
-    var imageUrl by remember { mutableStateOf<Uri?>(null)}
-    var bitmap by remember { mutableStateOf<Bitmap?>(null)}
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Librry
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
-    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()){ uri: Uri? ->
-        imageUrl = uri
-
-        val uuid = UUID.randomUUID()
-        val imageName = "$uuid.jpg"
-
-        val reference = storage.reference
-        val gorselReference = reference.child("images").child(imageName)
-        gorselReference.putFile(imageUrl!!).addOnSuccessListener { task->
-            Toast.makeText(context, "done", Toast.LENGTH_LONG).show()
-            val uploadedImage = storage.reference.child("images").child(imageName).downloadUrl.addOnSuccessListener { uri->
-                val downloadUrl = uri.toString()
-                Toast.makeText(context, downloadUrl, Toast.LENGTH_LONG).show()
-            }
-        }.addOnFailureListener { ex->
-            Toast.makeText(context, ex.localizedMessage, Toast.LENGTH_LONG).show()
-        }
-    }
-
-    Column {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .background(Purple500),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(text = "pick galerry image")
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray)
-                .padding(10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
-            imageUrl?.let {
-                if (Build.VERSION.SDK_INT < 28){
-                    bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
-                }else {
-                    val source = ImageDecoder.createSource(context.contentResolver, it)
-                    bitmap = ImageDecoder.decodeBitmap(source)
-                }
-                bitmap.let { bitmap ->
-                    Image(
-                        bitmap = bitmap!!.asImageBitmap(),
-                        contentDescription = "nullll",
-                        modifier = Modifier.size(400.dp)
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.padding(20.dp))
-            Button(
-                onClick = {
-                    launcher.launch("image/*")
-                }
-            ) {
-                Text(text = "click meee")
-            }
+    BottomNavigation {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
         }
     }
 }
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+        composable(route = BottomBarScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Librry.route) {
+            LibrryScreen()
+        }
+    }
+}
+
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : BottomBarScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+
+    object Librry : BottomBarScreen(
+        route = "librry",
+        title = "librry",
+        icon = Icons.Default.Person
+    )
+}
+
+@Composable
+fun HomeScreen(){
+    Text(text="home screen")
+}
+
+@Composable
+fun LibrryScreen(){
+    Text(text="Library screen")
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /**
