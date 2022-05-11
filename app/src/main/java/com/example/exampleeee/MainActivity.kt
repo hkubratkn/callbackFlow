657f5f2bba32171d457309cc892d4edcde6a477c
CASPER <hkubratkn@gmail.com>
2022-05-11 20:08:52 +0300

    imagePicker

--- app/src/main/java/com/example/exampleeee/MainActivity.kt
+++ app/src/main/java/com/example/exampleeee/MainActivity.kt
@@ -1,25 +1,129 @@
 package com.example.exampleeee
 
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.example.exampleeee.ui.theme.ExampleeeeTheme
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.provider.MediaStore
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
+import com.example.exampleeee.ui.theme.Purple500
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ExampleeeeTheme {
+                PickPhoto()
+            }
+        }
+    }
+}
+
+@Composable
+fun PickPhoto(){
+    val context = LocalContext.current
+    var imageUrl by remember { mutableStateOf<Uri?>(null)}
+    var bitmap by remember { mutableStateOf<Bitmap?>(null)}
+
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()){ uri: Uri? ->
+        imageUrl = uri
+    }
+
+    Column {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(Purple500),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "pick galerry image")
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray)
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            imageUrl?.let {
+                if (Build.VERSION.SDK_INT < 28){
+                    bitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, it)
+                }else {
+                    val source = ImageDecoder.createSource(context.contentResolver, it)
+                    bitmap = ImageDecoder.decodeBitmap(source)
+                }
+                bitmap.let { bitmap ->
+                    Image(
+                        bitmap = bitmap!!.asImageBitmap(),
+                        contentDescription = "nullll",
+                        modifier = Modifier.size(400.dp)
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.padding(20.dp))
+            Button(
+                onClick = {
+                    launcher.launch("image/*")
+                }
+            ) {
+                Text(text = "click meee")
+            }
+        }
+    }
+}
+
+
+/**
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings.Global.getString
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
 import com.example.exampleeee.ui.theme.ExampleeeeTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -133,12 +237,43 @@ fun AuthScreen(navController: NavController) {
 @Composable
 fun InsideScreen(navController: NavController){
     val auth = Firebase.auth
+    val db = Firebase.firestore
+    val currentUserUid = auth.currentUser!!.uid
+    val context = LocalContext.current
+
+    var date by remember{ mutableStateOf("") }
+    var mail by remember { mutableStateOf("") }
+    var photo by remember { mutableStateOf("") }
     Column (
         modifier=Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        db.collection("User").document(currentUserUid).addSnapshotListener { value, error ->
+            if (error!=null){
+                Toast.makeText(context, error.localizedMessage, Toast.LENGTH_LONG).show()
+            }else{
+                if (value != null){
+                    if (value.exists()){
+                        date = value.get("date") as String
+                        mail = value.get("email") as String
+                        photo = value.get("photo") as String
+                    }
+                }
+            }
+        }
         Text(text="inside")
+        Text(text = date)
+        Text(text = mail)
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp),
+            contentAlignment = Alignment.Center
+        ){
+            val painter = rememberImagePainter(data = "$photo.jpg", builder = {})
+            Image(painter = painter, contentDescription = null)
+        }
         Button(
             onClick = {
                 auth.signOut()
@@ -153,9 +288,7 @@ fun InsideScreen(navController: NavController){
 
 
 
-
-
-
+*/
 /**
 fun FirestoreScreen(){
     val db = Firebase.firestore
