25d0b519267cc886f1bc9e001f4189d64ac1723f
kubra tekin <kubra4231@outlook.com>
2022-06-18 18:28:06 +0300

    sondeneme

--- app/src/main/java/com/example/exampleeee/ui/theme/Color.kt
+++ app/src/main/java/com/example/exampleeee/ui/theme/Color.kt
@@ -6,16 +6,3 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-
-val background = Color(0xFFF5F5F5)
-
-val veryLightGray = Color(0xFFEFEFF0)
-val darkGray = Color(0xFF323233)
-
-val behaviourBackground = Color(0xFFf3d0d4)
-val behaviourTextColor = Color(0xFFf79ba9)
-val elderTextColor = Color(0xFF0ca66d)
-val elderBackground = Color(0xFF07ed98)
-val purpleButtonLight = Color(0xFFDDD6F7)
-val orangeButtonLight = Color(0x29FCC064)
-val orangeText = Color(0xFFF4A325)
