8856d0c2cf87bac97f34a2fba061071629166a10
kubra tekin <kubra4231@outlook.com>
2022-08-10 11:37:42 +0300

    app open ads

--- app/src/main/java/com/example/exampleeee/MainActivity.kt
+++ app/src/main/java/com/example/exampleeee/MainActivity.kt
@@ -3,44 +3,13 @@ package com.example.exampleeee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.example.exampleeee.data.MyService
-import com.example.exampleeee.ui.theme.ExampleeeeTheme
+import androidx.compose.material.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            ExampleeeeTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    val context = LocalContext.current
-                    Button(
-                        onClick = {
-                            MyService.startService(context, "service start")
-                        }
-                    ) { Text(text = "start") }
-                    Button(
-                        onClick = {
-                            MyService.stopService(context)
-                        }
-                    ){ Text(text = "stop") }
-
-                }
-            }
-
+            Text(text = "main")
         }
     }
-
 }
