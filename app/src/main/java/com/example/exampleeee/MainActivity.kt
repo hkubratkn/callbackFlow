16c625e739e691cfff3bd7eb2b33ebd24d5b91ad
kubra tekin <kubra4231@outlook.com>
2022-08-25 10:06:06 +0300

    callbackflow

--- app/src/main/java/com/example/exampleeee/MainActivity.kt
+++ app/src/main/java/com/example/exampleeee/MainActivity.kt
@@ -1,110 +1,108 @@
 package com.example.exampleeee
 
 import android.os.Bundle
-import android.os.Parcelable
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgs
-import androidx.navigation.navArgument
-import kotlinx.parcelize.Parcelize
+import com.example.exampleeee.Constants.CHAT_COLLECTION
+import com.example.exampleeee.Constants.DATE_FIELD
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            ChatScreen()
+        }
+    }
+}
 
-            val navController = rememberNavController()
+@Composable
+private fun ChatScreen(
+    chatScreenViewModel: ChatScreenViewModel = viewModel(
+        factory = ChatScreenViewModelFactory(
+            chatId = "it",
+            chatScreenRepository = ChatScreenRepository()
+        )
+    )
+) {
 
-            val sharedViewModel: SharedViewModel = viewModel()
+    val chatList = chatScreenViewModel.getChat.collectAsState().value
+    Log.d("chat", chatList.toString())
 
-            NavHost(
-                navController = navController,
-                startDestination = Screen.OneS.route
-            ) {
-                composable(route = Screen.OneS.route) {
-                    OneScreen(navController = navController, sharedViewModel = sharedViewModel)}
-                composable(route = Screen.SecondS.route){
-                    SecondScreen(navController = navController, sharedViewModel = sharedViewModel)
+}
+
+class ChatScreenViewModel(
+    private val chatId: String,
+    private val chatScreenRepository: ChatScreenRepository
+): ViewModel(){
+
+    private val _getChat = MutableStateFlow<List<Chat>>(emptyList())
+    var getChat: StateFlow<List<Chat>> = _getChat
+
+    init {
+        viewModelScope.launch {
+            chatScreenRepository.getChatList(chatId)
+                .collect { items ->
+                    _getChat.value = items
+                }
+        }
+    }
+
+}
+
+class ChatScreenViewModelFactory(
+    private val chatId: String,
+    private val chatScreenRepository: ChatScreenRepository
+): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = ChatScreenViewModel(chatId, chatScreenRepository) as T
+}
+
+class ChatScreenRepository() {
+
+    fun getChatList(chatId: String): Flow<List<Chat>> {
+        return Firebase.firestore.collection(CHAT_COLLECTION)
+            .document(chatId)
+            .collection(chatId)
+            .orderBy(DATE_FIELD, Query.Direction.ASCENDING)
+            .limit(10)
+            .snapshotFlow()
+            .map { querySnapshot ->
+                querySnapshot.documents.map {
+                    it.toObject(Chat::class.java)!!
                 }
             }
-
-        }
     }
-}
 
-@Composable
-fun OneScreen(
-    navController: NavController,
-    sharedViewModel: SharedViewModel
-) {
-    Column(modifier = Modifier.fillMaxSize()){
-        Text(text = "screen_one")
-        Button(
-            onClick = {
-                val person = Person(firstName = "ffif", lastName = "llsl")
-                sharedViewModel.addPerson(person)
-                navController.navigate(Screen.SecondS.route)
+    private fun Query.snapshotFlow(): Flow<QuerySnapshot> = callbackFlow {
+        val listenerRegistration = addSnapshotListener { value, error ->
+            if (error != null) {
+                close()
+                return@addSnapshotListener
             }
-        ){Text(text = "go")}
-    }
-}
-
-@Composable
-fun SecondScreen(
-    navController: NavController,
-    sharedViewModel: SharedViewModel
-) {
-    val person = sharedViewModel.person
-    LaunchedEffect(key1 = person ) {
-        if (person != null) {
-            Log.d("eede", "${person.lastName}")
-            Log.d("eede", "${person.firstName}")
-        }
-    }
-
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "second_screen")
-        Button(
-            onClick = {
-                navController.navigate(Screen.OneS.route)
+            if (value != null) {
+                trySend(value)
             }
-        ){
-            Text(text = "back")
+        }
+        awaitClose {
+            listenerRegistration.remove()
         }
     }
 }
 
-class SharedViewModel: ViewModel() {
 
-    var person by mutableStateOf<Person?>(null)
-        private set
-
-    fun addPerson(newPerson: Person) {
-        person = newPerson
-    }
-}
-
-
-sealed class Screen(val route: String) {
-    object OneS: Screen(route = "one_scren")
-    object SecondS: Screen(route = "second_screen")
-}
-
-@Parcelize
-class Person(
-    val firstName: String,
-    val lastName: String
-): Parcelable
+data class Chat(
+    var text: String ?= null,
+    var who: String ?= null,
+    @ServerTimestamp
+    var date: Date? = null
+)
---
+++ app/src/main/java/com/example/exampleeee/utils.kt
@@ -0,0 +1,9 @@
+package com.example.exampleeee
+
+object Constants {
+    const val AGE_FIELD = "age"
+    const val CHAT_COLLECTION = "Chat"
+    const val COMPLETED_FIELD = "completed"
+    const val DATE_FIELD = "date"
+    const val EMAIL_FIELD = "email"
+}
